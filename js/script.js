2eee30d8df103b390f0876ce1fc20bfaafb6d7fa
Carlos Magno <32673318+carlosmagno02@users.noreply.github.com>
2020-11-27 05:45:09 -0300

    cartas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 $(function(){
-    $('mobile').on('click' , function(){
-        var listaMenu = $('mobile ul');
-        listaMenu.slideToggle();    
+    $('.mobile').on('click', function(){
+        var listamenu = $('nav.mobile ul');
+        //Abrir menu atraves do fadein
+        /*if(listamenu.is(':hidden') == true)
+        listamenu.fadeIn();
+        else
+        listamenu.fadeOut();*/
+
+        //abrir e fechar menu sem efeitos
+        /*if(listamenu.is(':hidden') == true)
+        listamenu.show();
+        else
+        listamenu.hide();*/
+        
+        if(listamenu.is(':hidden') == true){
+        var icone = $('.botao-menu-mobile').find('i');
+        icone.removeClass('fa-bars');
+        icone.addClass('fa-times')
+        listamenu.slideToggle();
+        }else{
+        var icone = $('.botao-menu-mobile').find('i');
+        listamenu.slideToggle();
+        icone.removeClass('fa-times');
+        icone.addClass('fa-bars')
+        }
     })
 })
